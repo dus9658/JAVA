3a48ad5ffcc74b6b39cc026eb23ea64f9809d2f9
dus9658 <120646716+dus9658@users.noreply.github.com>
2023-06-30 16:39:46 +0900

    수정기능 추가

--- UserManagement/src/user/UserMain.java
+++ UserManagement/src/user/UserMain.java
@@ -1,5 +1,7 @@
 package user;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class UserMain {
@@ -10,8 +12,8 @@ public class UserMain {
 
         while (true) {
             int menuId = userUI.menu();
-            if(menuId == 3){
-                System.out.println("프로그램을 종료합니다");
+            if(menuId == 5){
+                System.out.println("프로그램을 종료합니다.");
                 userDao.saveUser(users);
                 break;
             }else if(menuId == 1){
@@ -20,6 +22,23 @@ public class UserMain {
                 System.out.println("등록되었습니다.");
             }else if (menuId == 2){
                 userUI.printUserList(users);
+            } else if(menuId == 3){
+                String email = userUI.inputEmail();
+                int findIndex = -1;
+                for(int i = 0; i < users.size(); i++) {
+                    User u = users.get(i);
+                    if(u.getEmail().equals(email)){
+                        findIndex = i;
+                        break;
+                    }
+                }
+                if(findIndex != -1) {
+                    User updateUser = userUI.inputUser(email);
+                    users.remove(findIndex);
+                    users.add(updateUser);
+                } else {
+                    System.out.println("해당 이메일에 맞는 회원이 없습니다.");
+                }
             }
             System.out.println();
         }
--- UserManagement/src/user/UserUI.java
+++ UserManagement/src/user/UserUI.java
@@ -14,8 +14,9 @@ public class UserUI {
     public int menu(){
         System.out.println("1. 회원등록");
         System.out.println("2. 회원 목록 보기");
-        System.out.println("3. 종료");
-
+        System.out.println("3. 회원 정보 수정");
+        System.out.println("4. 회원 정보 삭제");
+        System.out.println("5. 종료");
         int menuId = -1;
         try {
             String line = br.readLine();
@@ -24,6 +25,33 @@ public class UserUI {
             ex.printStackTrace();
         }
         return menuId;
+    }
+
+    public String inputEmail(){
+        System.out.println("수정할 회원의 email을 입력하세요.");
+        String email = "";
+        try {
+            email = br.readLine();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return email;
+    }
+
+    public User inputUser(String email){
+        try {
+            System.out.println(email+" 회원의 정보를 수정합니다.");
+            System.out.println("이름을 입력하세요");
+            String name = br.readLine();
+            System.out.println("생년을 입력하세요");
+            int birthYear= Integer.parseInt(br.readLine());
+
+            User user = new User(email, name, birthYear);
+            return user;
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
     }
 
     public User regUser() {
